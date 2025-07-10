868cee2394fefeaf4d4ded1faad2e39518ec7831
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-11 00:00:55 +0300

    fix: improve error handling and formatting in login thunk

--- src/features/auth/authThunk.js
+++ src/features/auth/authThunk.js
@@ -4,13 +4,13 @@ export const login = createAsyncThunk(
     'auth/login',
     async ({ email, password }, { rejectWithValue }) => {
         try {
-            const response = await loginRequest({email, password});
+            const response = await loginRequest({ email, password });
             return {
                 ...response.data.user,
                 jwt: response.data.jwt
             }
         } catch (err) {
-            const msg = err.response?.data?.message || "Failed to fetch";
+            const msg = err.response.data.message || "Failed to login"
             return rejectWithValue(msg);
         }
     }
